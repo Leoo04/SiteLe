1d69fa5c842c91ddf86dc81a20802869c5b11055
Leoo04 <147108380+Leoo04@users.noreply.github.com>
2024-02-15 20:41:42 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,21 @@
+var titulo = document.querySelector(".titulo");
+
+document.querySelector(".Total1").textContent =
+document.querySelector(".Valor1").textContent *
+document.querySelector(".qtd1").textContent
+
+document.querySelector(".Total2").textContent =
+document.querySelector(".Valor2").textContent *
+document.querySelector(".qtd2").textContent
+
+document.querySelector(".Total3").textContent =
+document.querySelector(".Valor3").textContent *
+document.querySelector(".qtd3").textContent
+
+document.querySelector(".Total4").textContent =
+document.querySelector(".Valor4").textContent *
+document.querySelector(".qtd4").textContent
+
+document.querySelector(".Total5").textContent =
+document.querySelector(".Valor5").textContent *
+document.querySelector(".qtd5").textContent
